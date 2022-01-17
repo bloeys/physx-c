5673431fb83e6e51f8cdfaf6cc79e2728b337e9c
bloeys <bloeys@outlook.com>
2022-01-17 20:33:21 +0400

    Scene simulate and fetch results

--- CPxScene.cpp
+++ CPxScene.cpp
@@ -11,8 +11,19 @@ CPxPvdSceneClient* CPxScene_getScenePvdClient(CPxScene* cs)
 
 void CPxScene_addActor(CPxScene* cScene, CPxActor* cActor)
 {
+
 	static_cast<physx::PxScene*>(cScene->obj)->
 		addActor(*static_cast<physx::PxActor*>(cActor->obj));
+}
+
+void CPxScene_simulate(CPxScene* cs, CPxReal elapsedTime)
+{
+	static_cast<physx::PxScene*>(cs->obj)->simulate(elapsedTime);
+}
+
+bool CPxScene_fetchResults(CPxScene* cs, bool block, CPxU32* errorState)
+{
+	return static_cast<physx::PxScene*>(cs->obj)->fetchResults(block, errorState);
 }
 
 void CPxScene_release(CPxScene* cs)
