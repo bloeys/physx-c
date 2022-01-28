6e170f641e11140e36a83661aa309cdb9eb6a334
bloeys <bloeys@outlook.com>
2022-01-28 06:36:00 +0400

    Add ability to set scratch buffer on scene

--- CPxScene.cpp
+++ CPxScene.cpp
@@ -17,7 +17,7 @@ void CPxScene_addActor(CPxScene* cScene, CPxActor cActor)
 
 void CPxScene_simulate(CPxScene* cs, CPxReal elapsedTime)
 {
-	static_cast<physx::PxScene*>(cs->obj)->simulate(elapsedTime);
+	static_cast<physx::PxScene*>(cs->obj)->simulate(elapsedTime, 0, cs->scratchBuffer, cs->scratchBufferSize, true);
 }
 
 bool CPxScene_fetchResults(CPxScene* cs, bool block, CPxU32* errorState)
@@ -25,8 +25,35 @@ bool CPxScene_fetchResults(CPxScene* cs, bool block, CPxU32* errorState)
 	return static_cast<physx::PxScene*>(cs->obj)->fetchResults(block, errorState);
 }
 
+void CPxScene_setScratchBuffer(CPxScene* cs, uint32_t multiplesOf16k)
+{
+	physx::PxDefaultAllocator pa;
+
+	if (cs->scratchBuffer != NULL)
+	{
+		pa.deallocate(cs->scratchBuffer);
+		cs->scratchBuffer = NULL;
+		cs->scratchBufferSize = 0;
+	}
+
+	if (multiplesOf16k == 0)
+		return;
+
+	cs->scratchBufferSize = 1024 * 16 * multiplesOf16k;
+	cs->scratchBuffer = pa.allocate(cs->scratchBufferSize, 0, 0, 0);
+}
+
+
 void CPxScene_release(CPxScene* cs)
 {
 	static_cast<physx::PxScene*>(cs->obj)->release();
+
+	physx::PxDefaultAllocator pa;
+	if (cs->scratchBuffer != NULL)
+	{
+		pa.deallocate(cs->scratchBuffer);
+		cs->scratchBuffer = NULL;
+	}
+
 	free(cs);
 }
